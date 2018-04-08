758e30393e14b8e80f4c6fca6f6851f2bd828277
amiturfriend007 <amiturfriend007@yahoo.com>
2018-04-08 14:40:49 +0530

    posetron repo

---
+++ js/contact_me.php
@@ -0,0 +1,42 @@
+<?php
+// Check for empty fields
+
+require_once('././PHPMailer/PHPMailerAutoload.php');
+
+$mail=new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth=true;
+$mail->SMTPSecure='ssl';
+$mail->Host='smtp.gmail.com';
+$mail->Port='465';
+$mail->isHTML();
+$mail->Username='dey7792@gmail.com';
+$mail->Password='';
+
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'dey7792@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@posetron.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+$result=$mail->Send();
+$message=$result?'sent':'Fail';
+echo $message;
+return true;         
+?>
---
+++ js/mail.js
@@ -0,0 +1,36 @@
+/*(function(req, res){
+  var nodemailer = require('nodemailer');
+window.document.writeln("<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js'></script>");
+console.log(req);
+
+//var app = require('angular.js');
+/*var app1=angular.module('myApp', []);
+app1.controller('myCtrl', function($scope) {
+  $scope.firstname = "John";
+  $scope.message = "hi how are you!!";
+});
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dey7792@gmail.com',
+    pass: 'rattlesnake#block'
+  }
+});
+
+var mailOptions = {
+  from: 'dey7792@gmail.com',
+  to: 'dey7792@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'Hi there'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+})();*/
